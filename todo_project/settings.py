d9a71ab74de4f7b12f064f2fb5073917be0f2f49
Nguyen Huu Phuc <phucnguyenhuu6504@gmail.com>
2025-03-06 21:38:42 +0700

    Package with Docker (dockerfile and docker-compose)

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "NAME": "todoapp",
         "USER": "postgres",
         "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
